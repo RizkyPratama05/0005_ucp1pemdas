95b64e06648fbe8827b7e637a00d79b53037223c
RizkyPratama05 <rizkypp428@gmail.com>
2024-05-05 19:04:28 +0700

    first commit

---
+++ ucp1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa {
+    string nama;
+    int nilai;
+};
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nama Mahasiswa: ";
+    cin >> mhs.nama;
+    cout << "Nilai Mahasiswa: ";
+    cin >> mhs.nilai;
+}
+
+void nilai() {
+    int i;
+    string nama;
+
+    if (int i = 70; i < 70)
+    {
+        cout << " . " << "Ditolak" << endl;
+    }
+
+    if (int i = 80; i > 70)
+    {
+        cout << " . " << "Diterima" << endl;
+    }
+}
+
+
+
+
+// 1. int,float,strutc
+// 2.
+// 3. contoh implementasi dari struct yaitu seperti mengetahui data mahasiswa beserta alamatnya
+// 4. 
+// 5. contoh implementasi dari looping yaitu seperti penghitungan harga pada telor dan indomie
